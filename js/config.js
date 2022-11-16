90ed6b41f756605bfeb4b7a8fb68dac924b92dfe
LE0xUL <LE0@0x3L.com>
2022-11-16 16:32:19 -0500

    Update config to 0.1.0 version

--- js/config.js
+++ js/config.js
@@ -3,20 +3,20 @@ const develNet = false;
 const cfgLocalNet = {
     addrApiCLH : "0x3194cBDC3dbcd3E11a07892e7bA5c3394048Cc87",
     addrCLFactory : "0x420b1099B9eF5baba6D92029594eF45E19A04A4A",
-    addrCLBeacon : "0x4973215186cd002578B4d92f54B868550627f2a4",
+    addrCLBeacon : "0x9E4c14403d7d9A8A782044E86a93CAE09D7B2ac9",
     pKeyPayeer : "0x840bdb63e4e065597a3f5d5e5a3eed7b6b858400f2e262e83065bcec77049194", //BRW#99
     domEIP712Name: "CLHouse",
-    domEIP712Version: "0.0.10",
+    domEIP712Version: "0.1.0",
     domEIP712IdChain: "0x539",
 };
 
 const cfgTestNet = {
-    addrApiCLH : "0xa0ccC5f57812E699929C53B2A5C536Da12CD54e3",
-    addrCLFactory : "0x6def2797169299C12EA7e24C042de739234e2d95",
-    addrCLBeacon : "0x4973215186cd002578B4d92f54B868550627f2a4",
+    addrApiCLH : "0x64e165A55E43ebddeC5000D1BE4aE35E17cB7bfD",
+    addrCLFactory : "0xB049136321DE7113b19F4F15f8F3fb7375212D63",
+    addrCLBeacon : "0xe8F4087322dad5450C538670688F8c5e64775786",
     pKeyPayeer : "0x840bdb63e4e065597a3f5d5e5a3eed7b6b858400f2e262e83065bcec77049194", //BRW#99
     domEIP712Name: "CLHouse",
-    domEIP712Version: "0.0.10",
+    domEIP712Version: "0.1.0",
     domEIP712IdChain: "0x5",
 };
 
